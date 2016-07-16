d561d463a9ea2bc61679fc33383108a17ca493c9
Eric Crahen <eric.crahen@snapchat.com>
2016-07-16 10:23:44 -0700

    /

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 ./build.sh
-gsutil cp build/gradle-3.0-snapchat.zip
+gsutil cp build/gradle-3.0-snapchat.zip gs://snapengine-builder/gradle-3.0-snapchat.zip
 gsutil -D setacl public-read gs://snapengine-builder/gradle-3.0-snapchat.zip
