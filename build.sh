b2961746806f9566e0172ae281ff9503d4cb1f7e
Eric <Eric.Crahen@snapchat.com>
2016-07-16 09:58:27 -0700

    make sure PR test (example) uses locally built gradle (#7)

--- build.sh
+++ build.sh
@@ -1,16 +1,34 @@
 #!/bin/bash -ex
+
+# Move the snapchat.gradle file we haven't deprecated yet
+OLDCONFIG=$HOME/.gradle/init.d
+BAKCONFIG=$HOME/.gradle/init.d.old
+trap "test ! -e $OLDCONFIG && test -e $BAKCONFIG && mv $BAKCONFIG $OLDCONFIG" 0
+[ -e "$OLDCONFIG" ] && rm -rf "$BAKCONFIG" && mv "$OLDCONFIG" "$BAKCONFIG"
+
+
+# Setup the output location, remove stale artifacts
 export DISTRIBUTION=$PWD/build/gradle-3.0-snapchat.zip
-rm -rf $DISTRIBUTION
+[ -z "$GRADLE_USER_HOME" ] && GRADLE_USER_HOME=$HOME
 
-./gradlew install -Pgradle_installPath=build/distribution
-cd build/distribution
-zip -r $DISTRIBUTION .
+rm -f "$DISTRIBUTION"
+rm -rf "$GRADLE_USER_HOME"/.gradle/wrapper/dists/*
 
-# TODO: Set gradle properties distirubutionUrl to point to file
+./gradlew outputsZip -Pgradle_installPath=build/distribution
+ln -sf $PWD/build/distributions/gradle-3.0-snapshot-*bin.zip "$DISTRIBUTION"
 
 # Build a project that consumes this artifact (from a local url)
 # to test before passing the build
-#cd example
-#./gradlew build
+ln -sf "$DISTRIBUTION" example/gradle/wrapper
+ln -sf "$DISTRIBUTION" example-with-no-fallback/gradle/wrapper
 
+# Run a test that uses GCS and then falls back to artifactory
+cd example
+# ./gradlew build -S --info
+cd ..
+
+# Run a test that uses GCS and does not fall back to artifactory
+cd example-with-no-fallback
+# TODO: uncomment to test plugin
+# ./gradlew build -S --info
 cd ..
